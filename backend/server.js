aff0011d4019b4cd37baefdf51f6583a24858096
Tanel Pauls <paulstanel@protonmail.com>
2025-04-10 16:03:55 +0300

    made starter files for mariaDB project

---
+++ backend/server.js
@@ -0,0 +1,2 @@
+const PORT = 3013;
+console.log(`Server is running on port ${PORT}.`);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>E-õpe 4: Kokkuvõttev ülesanne (MariaDB)</h1>
+    </>
+  );
+}
+
+export default App;
